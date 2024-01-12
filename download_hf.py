624e4e9e4f64c6c74908100c811ee6140c62c760
杨亚杰 <39705429+JieGenius@users.noreply.github.com>
2024-01-12 22:42:30 +0800

    fix: model checkpoint path

--- download_hf.py
+++ download_hf.py
@@ -1,12 +1,18 @@
 import os
 from modelscope import snapshot_download, AutoModel, AutoTokenizer
+from pathlib import Path
 
 # 设置环境变量
 os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
 
+
 def download():
-# 下载模型
-    os.system('huggingface-cli download --resume-download sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2 --local-dir model/sentence-transformer')
+    # 下载模型
+    os.system(
+        'huggingface-cli download --resume-download sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2 --local-dir model/sentence-transformer')
+
 
 def download_model(model_path):
-    model_dir = snapshot_download('Shanghai_AI_Laboratory/internlm-chat-7b', cache_dir=model_path, revision='v1.0.3')
+    assert model_path.endswith('Shanghai_AI_Laboratory/internlm-chat-7b')
+    model_dir = snapshot_download('Shanghai_AI_Laboratory/internlm-chat-7b',
+                                  cache_dir=str(Path(model_path).parent.parent), revision='v1.0.3')
